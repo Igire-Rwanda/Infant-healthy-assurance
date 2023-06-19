73b418350b3aa197e35c33a6aca5ff9ce3f114cf
Rosinemukandayisenga <rosinemuka2020@gmail.com>
2023-06-19 11:42:07 +0200

    this is broadcastpage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-<<<<<<< HEAD
+
 import React, { useState } from 'react';
 import { StyleSheet,setText, View, Platform } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import Appointment from '../src/Appointment.js'
+// import Appointment from '../src/Appointment.js'
+import Broadcast from './Pages/broadcast/broadcast'
 
 const App = () => {
-    <Appointment/>
-=======
-import "./App.css";
+return (
+  
+  <Broadcast/>
+    // <Appointment/>
 
-import Home from "./Pages/index.jsx"
-function App() {
-  return (
-    <div className="App">
+// import "./App.css";
+
+// import Home from "./Pages/index.jsx"
+// function App() {
+//   return (
+//     <div className="App">
     
-      <Home/>
-    </div>
-  );
->>>>>>> ef8fb0694620403d5679a295eb75320360be25be
+//       <Home/>
+//     </div>
+//   );
+// >>>>>>> ef8fb0694620403d5679a295eb75320360be25be
+);
 }
 export default App;
---
+++ src/Pages/broadcast/broadcast.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './broadcast.css'
+const Broadcasting=()=>{
+    return(
+        <>
+        <h1>This is a broadcast page</h1>
+        </>
+    )
+}
+export default Broadcasting;
