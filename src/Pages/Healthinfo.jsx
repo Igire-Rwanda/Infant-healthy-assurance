f296090266b49ca519b89a1170b88c78ac051a8b
Clairon <niwemfurac@gmail.com>
2023-06-21 15:27:22 +0200

    adding the integration part

--- src/Pages/Healthinfo.jsx
+++ src/Pages/Healthinfo.jsx
@@ -1,26 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import DoctorLayout from "../Layout/DoctorLayout";
 import "../styles/sidemenu.css"
 export default function Healthinfo(){
+    const realvalues={
+        category:"",
+        title:"" ,
+        image:"",
+        content:"",
+        author:"",
+    };
+    const[formvalues,setformvalues] =useState(realvalues);
+    const [isSubmited,setSubmited] =useState(false);
+    const handlechange =(e) =>{
+        const {name,value} = e.target;
+        setformvalues({...formvalues,[name]:value });
+    };
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        sendData ();
+        setSubmited(true);
+    };
+    const sendData = async ()=>{
+        try{
+            const response = await fetch(
+               "http://localhost:5000/api/v1/healthinfo/create",
+              {
+                 method:"post",
+                 headers:{
+                    "content-Type":"application/json",
+                 },
+                 body:JSON.stringify(formvalues),     
+              }  
+            );
+            if (response.ok){
+                window.location.href="/healthcare";
+            }else{
+                throw new Error("failed to save data");
+            }
+        }catch(error){
+           console.error("ann error occurred.please try again later",error);
+        }
+    };
+
     return(
         <DoctorLayout>
             <div className="healthinfoBody">
-                <div className="healthinfo2">
-                   <div className="title">
-                        <input type="text" placeholder="Title" id="titlein" /> 
-                        <input type="number" placeholder="Age" id="age" /> 
-                    </div>
-                    <div className="image">
-                        <p>Image</p>
-                        <input type="file" placeholder="image" />
                 
-                    </div>
-                    <div className="content">
-                        <input type="text" placeholder="content" />
-                    </div>
-                    <div>
-                        <button className="healthbtn">save</button>
-                    </div>
+                <div className="healthinfo2">
+                <pre>{JSON.stringify(formvalues,undefined,2)}</pre>
+                    <form onSubmit={handleSubmit}>
+                        <div className="title">
+                            <input type="text" placeholder="title" id="titlein" name="title" value={formvalues.title} onChange={handlechange} /> 
+                            <input type="number" placeholder="Age" id="age" /> 
+                        </div>
+                        <div className="image">
+                            <p>Image</p>
+                            <input type="file" placeholder="image" name="image" value={formvalues.image} onChange={handlechange}/>
+                    
+                        </div>
+                        <div className="content">
+                            <input type="text" placeholder="content" name="content" value={formvalues.content} onChange={handlechange} />
+                        </div>
+                        <div>
+                            <button className="healthbtn" type="submit" >save</button>
+                        </div>
+                    </form>
                </div>
             </div>
         </DoctorLayout>
