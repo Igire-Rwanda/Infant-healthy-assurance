2a5a15a94ec1a8fa36c8b08c3bbe84e8345953bb
Rene Promesse <nrenejordan1@gmail.com>
2023-06-28 01:43:40 +0200

    adds the sidebar

--- src/Components/SideMenu/SideMenu.jsx
+++ src/Components/SideMenu/SideMenu.jsx
@@ -1,31 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SideElement from "./Oneforprop";
 import icon1 from "../../Images/home.png";
-import icon6 from "../../Images/square.png";
 import icon2 from "../../Images/broadcast.png";
 import icon3 from "../../Images/registration.png";
 import icon4 from "../../Images/appointments.png";
 import icon5 from "../../Images/healthcare.png";
+import icon6 from "../../Images/square.png";
 import icon7 from "../../Images/bbell.png";
-import Oneforprop from "./Oneforprop.jsx";
-import { Link } from "react-router-dom";
 
 function SideMenu() {
   return (
-    <div>
+    <div className="sideMenuContainer">
       <div className="upper">
-        <Oneforprop icon={icon6} name="Dashboard" link="/dashboard" />
+        <SideElement icon={icon6} name="Dashboard" link="/dashboard" />
       </div>
       <div className="lower">
-        <Oneforprop icon={icon1} name="Home" link="/" />
-        <Oneforprop icon={icon2} name="Broadcast" link="/broadcast" />
-        <Oneforprop icon={icon3} name="Registration" link="/register" />
-        <Oneforprop icon={icon4} name="Appointment" link="/appointment" />
-        <Oneforprop icon={icon7} name="Notification" link="/notification" />
-        <Oneforprop icon={icon5} name="Heathcare" link="/healthcare" />
+        <SideElement icon={icon1} name="Home" link="/" />
+        <SideElement icon={icon2} name="Broadcast" link="/broadcast" />
+        <SideElement icon={icon3} name="Registration" link="/register" />
+        <SideElement icon={icon4} name="Appointment" link="/appointment" />
+        <SideElement icon={icon7} name="Notification" link="/notification" />
+        <SideElement icon={icon5} name="Healthcare" link="/healthcare" />
       </div>
       <div className="button">
-        <button className="btn"><Link to="/">Logout</Link></button>
+        <button className="btn">
+          <Link to="/">Logout</Link>
+        </button>
       </div>
     </div>
   );
 }
+
 export default SideMenu;
