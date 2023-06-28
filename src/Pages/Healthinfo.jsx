6bb1721411e253633303ece2369969347c6d8c7d
Rene Promesse <nrenejordan1@gmail.com>
2023-06-28 08:55:00 +0200

    added the link for health info

--- src/Pages/Healthinfo.jsx
+++ src/Pages/Healthinfo.jsx
@@ -23,7 +23,7 @@ export default function Healthinfo(){
     const sendData = async ()=>{
         try{
             const response = await fetch(
-               "http://localhost:3500/api/v1/healthinfo/create",
+               "https://sore-puce-brown-bear-sock.cyclic.app/api/v1/healthinfo/create",
               {
                  method:"post",
                  headers:{
